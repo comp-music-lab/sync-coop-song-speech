6530e15147a980c99b07fe544a7b25c027df2f86
Patrick Savage <patsavagenz@gmail.com>
2024-05-20 16:36:18 +1200

    More pilot data

--- SongSpeechCooperation2024-03-20.R
+++ SongSpeechCooperation2024-03-20.R
@@ -27,7 +27,6 @@ library(dplyr)
 library(readr)
 library(forcats)
 library(psych)
-library(tidyr)
 
 source("R_rainclouds.R")
 source("summarySE.R")
@@ -45,31 +44,15 @@ head(summary_simdat)
 #file.data <- "https://raw.githubusercontent.com/compmusiclab/rhythm-coop/master/RhythmPilotData(Fig3).csv"
 
 #Load and pre-process pilot data
-df<-read_csv(file="QualtricsPilotData_Merged(2024-05-20).csv")
-df<-df[-c(1:2),c(17,19,40:46,52,54:56,62:65,71:74,80:83,89)]
-write.csv(df,"keydata.csv")
-rep_data<-read_csv(file="keydata.csv")
+rep_data<-read_csv(file="RhythmPilotData(2024-03-20).csv",
+                   col_types = cols(group = col_factor(levels = c("synchronous song", "synchronous speech", "asynchronous song", "asynchronous speech")), 
+                                    time = col_factor(levels = c("pre-intervention", "post-intervention"))))
+rep_data$score<-rowMeans(rep_data[,4:7])
 
 #Internal consistency analysis of individual cooperation variables (Cronbach's alpha)
 psych::alpha(rep_data[,4:7])
 
-rep_data$t1<-rowMeans(rep_data[,4:7])
-rep_data$t2<-rowMeans(rep_data[,8:11])
-rep_data$t3<-rowMeans(rep_data[,12:15])
-rep_data$t4<-rowMeans(rep_data[,16:19])
-rep_data$t5<-rowMeans(rep_data[,20:23])
-rep_data$t6<-rowMeans(rep_data[,24:27])
-data_long <- gather(rep_data, time, score, t1:t2, factor_key=TRUE)
-colnames(data_long)[1] <- "Participant"
-write.csv(data_long,"keydata_long.csv")
-
-rep_data<-read_csv(file="keydata_long.csv",
-                   col_types = cols(group = col_factor(levels = c("GS", "GC", "GR","NV")), 
-                                    time = col_factor(levels = c("t1", "t2"))))
-rep_data<-rep_data[,-1]
-
-
-#Plot all  groups
+#Plot all 4 groups
 sumrepdat <- summarySE(rep_data, measurevar = "score", groupvars=c("group", "time"))
 
 head(sumrepdat)
@@ -83,12 +66,67 @@ p11 <- ggplot(rep_data, aes(x = time, y = score, fill = group)) +
   geom_errorbar(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group, ymin = score_mean - sem, ymax = score_mean + sem), width = .05)+
   scale_colour_brewer(palette = "Dark2")+
   scale_fill_brewer(palette = "Dark2")+
-  ylim(0,100)+ 
-  ggtitle("pre-/post-intervention bonding")
+  ylim(1,7)+ 
+  ggtitle("Figure 11: Repeated Measures - Factorial (Extended)")
 
 ggsave('4Conditions.png', width = w, height = h)
 
 p11
+
+#Collapse to compare song vs speech:
+rep_data<-read_csv(file="RhythmPilotData(2024-03-20).csv",
+                   col_types = cols(group = col_factor(levels = c("synchronous song", "synchronous speech", "asynchronous song", "asynchronous speech")), 
+                                    time = col_factor(levels = c("pre-intervention", "post-intervention"))))
+rep_data$score<-rowMeans(rep_data[,4:7])
+rep_data$group<-ifelse(rep_data$group=="synchronous song"|rep_data$group=="asynchronous song","song","speech")
+
+sumrepdat <- summarySE(rep_data, measurevar = "score", groupvars=c("group", "time"))
+head(sumrepdat)
+
+p11 <- ggplot(rep_data, aes(x = time, y = score, fill = group)) +
+  geom_flat_violin(aes(fill = group),position = position_nudge(x = .1, y = 0), adjust = 1.5, trim = FALSE, alpha = .5, colour = NA)+
+  geom_point(aes(x = as.numeric(time)-.15, y = score, colour = group),position = position_jitter(width = .05), size = .25, shape = 20)+
+  geom_boxplot(aes(x = time, y = score, fill = group),outlier.shape = NA, alpha = .5, width = .1, colour = "black")+
+  geom_line(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group), linetype = 3)+
+  geom_point(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group), shape = 18) +
+  geom_errorbar(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group, ymin = score_mean - sem, ymax = score_mean + sem), width = .05)+
+  scale_colour_brewer(palette = "Dark2")+
+  scale_fill_brewer(palette = "Dark2")+
+  ylim(1,7)+ 
+  ggtitle("Figure 11: Repeated Measures - Factorial (Extended)")
+
+ggsave('SongVsSpeech.png', width = w, height = h)
+
+p11
+
+
+#Collapse to compare synchrony vs asynchrony:
+rep_data<-read_csv(file="RhythmPilotData(2024-03-20).csv",
+                   col_types = cols(group = col_factor(levels = c("synchronous song", "synchronous speech", "asynchronous song", "asynchronous speech")), 
+                                    time = col_factor(levels = c("pre-intervention", "post-intervention"))))
+rep_data$score<-rowMeans(rep_data[,4:7])
+rep_data$group<-ifelse(rep_data$group=="synchronous song"|rep_data$group=="synchronous speech","synchronous","asynchronous")
+
+sumrepdat <- summarySE(rep_data, measurevar = "score", groupvars=c("group", "time"))
+head(sumrepdat)
+
+p11 <- ggplot(rep_data, aes(x = time, y = score, fill = group)) +
+  geom_flat_violin(aes(fill = group),position = position_nudge(x = .1, y = 0), adjust = 1.5, trim = FALSE, alpha = .5, colour = NA)+
+  geom_point(aes(x = as.numeric(time)-.15, y = score, colour = group),position = position_jitter(width = .05), size = .25, shape = 20)+
+  geom_boxplot(aes(x = time, y = score, fill = group),outlier.shape = NA, alpha = .5, width = .1, colour = "black")+
+  geom_line(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group), linetype = 3)+
+  geom_point(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group), shape = 18) +
+  geom_errorbar(data = sumrepdat, aes(x = as.numeric(time)+.1, y = score_mean, group = group, colour = group, ymin = score_mean - sem, ymax = score_mean + sem), width = .05)+
+  scale_colour_brewer(palette = "Dark2")+
+  scale_fill_brewer(palette = "Dark2")+
+  ylim(1,7)+ 
+  ggtitle("Figure 11: Repeated Measures - Factorial (Extended)")
+
+ggsave('SynchronousVsAsynchronous.png', width = w, height = h)
+
+p11
+
+
 
 
 
