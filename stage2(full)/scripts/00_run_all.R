6bc201380acc588619de1f72cef4724553866afb
Zixuan Jia <zixuanjia523@gmail.com>
2026-09-22 19:26:50 +1200

    Update 00_run_all.R

--- stage2(full)/scripts/00_run_all.R
+++ stage2(full)/scripts/00_run_all.R
@@ -57,12 +57,12 @@ source(file = file.path(stage2scripts,'03b_summarySE.R'))
 #Create raincloud plots
 source(file = file.path(stage2scripts,'03c_raincloud.R'))
 
-##Run expectancy exploratory analyses (Table 2)
-source(file = file.path(stage2scripts,'05_exploratory_analysis-expectancy.R'))
-
 ##Run other exploratory analyses (Figs. 6 on)
 source(knitr::purl(file.path(stage2scripts, '04_exploratory_analyses.Rmd'),
                    output = tempfile(fileext = '.R'), quiet = TRUE))
+
+##Run expectancy exploratory analyses (Table 2)
+source(file = file.path(stage2scripts,'05_exploratory_analysis-expectancy.R'))
 
 ##Run confirmatory Bayesian analyses (Tables 1, S4, and S14)
 #Run analyses for Research Question 1 ("Does singing enhance social bonding?")
